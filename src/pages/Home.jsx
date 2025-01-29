388451a9472e4d38033f949200a7d8c12863cace
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-29 13:53:25 +0530

    complete project setup with Konva library and tailwind css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Stage, Layer, Rect, Circle } from 'react-konva';
+
+
+function Home() {
+  return (
+   <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect width={50} height={50} fill="red" />
+        <Circle x={200} y={200} stroke="black" radius={50} />
+      </Layer>
+    </Stage>
+  )
+}
+
+export default Home
