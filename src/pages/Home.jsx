243805497cd9efe4f4f196dbd8536c4484a5daaf
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-30 22:50:32 +0530

    add circle shape

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Stage, Layer, Rect, Circle } from 'react-konva';
 function Home() {
     const [shapes,setShapes]=useState([{}])
     const canvasRef=useRef(null)
+    const idRef=useRef(null)//store previous shape id 
 
     useEffect(()=>{
 
@@ -14,31 +15,66 @@ function Home() {
         let y=0;
         let height=0;
         let width=0
+        let isDrawing=false
 
 
         canvasREF.addEventListener('mousedown',(e)=>{
+          isDrawing=true
           x=e.clientX;
           y=e.clientY;
+
+           idRef.current = Date.now();
+
+            let shape = {
+          x: x,
+          y: y,
+          width: 0,
+          height: 0,
+          type: "rect",
+          fill: "red",
+          stroke: "white",
+          strokeWidth: 30,
+          opacity: 0.5,
+          id: idRef.current,
+        };
+
+        setShapes((p) => [...p, shape]);
         })
 
+         canvasREF.addEventListener("mousemove", (e) => {
+            if (!isDrawing) return;
+
+          setShapes((shapes) => {
+              return shapes.map((shape) => {
+              if (shape.id === idRef.current) {
+                shape.height = e.clientY - y;
+                shape.width = e.clientX - x;
+              }
+              return shape;
+            });
+        });
+      });
+
+
         canvasREF.addEventListener("mouseup",(e)=>{
+          isDrawing=false
           width=e.clientX-x
           height=e.clientY-y
 
           console.log("x is ", x, "y is ", y);
           console.log("height is ", height, "widht is ", width);
 
-          let shape = {
-            x: x,
-            y: y,
-            width: width,
-            height: height,
-            fill: "red",
-            type: "rect",
-            id: Date.now(),
-          };
+          // let shape = {
+          //   x: x,
+          //   y: y,
+          //   width: width,
+          //   height: height,
+          //   fill: "red",
+          //   type: "rect",
+          //   id: Date.now(),
+          // };
           
-          setShapes((prev)=>[...prev,shape])
+          // setShapes((prev)=>[...prev,shape])
         })
       
 
@@ -69,13 +105,6 @@ function Home() {
     }
   return (
     <>
-    {shapes.length===0?(
-      <p className='p-10 text-center text-[20px] font-semibold bg-slate-800 text-white'>
-        Welcome Excalidraw
-    </p>):(
-      <>
-      {/* kanva design part */}
-      <button onClick={drawShapes} className='p-[10px] bg-green-600 rounded-xl'>Draw Shapes</button>
         <Stage width={window.innerWidth} height={window.innerHeight} className='bg-slate-800' ref={canvasRef}>
         <Layer>
               {shapes.map((element)=>{
@@ -92,10 +121,7 @@ function Home() {
               })}
         </Layer>
         </Stage>
-        </>
-        )}
-    </>
+      </>
   )
 }
-
 export default Home
